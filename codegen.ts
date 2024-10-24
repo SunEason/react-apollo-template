9619fcaec107bc5b05842ded5eefaffd22f9724e
Eason Sun <ballentin133@outlook.com>
2024-10-24 23:14:09 +0800

    feat: 指定 documents 文件为 graphql 文件

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 const config: CodegenConfig = {
   overwrite: true,
   schema: 'http://localhost:3000/graphql',
-  documents: 'src/graphql/*.ts',
+  // documents: 'src/graphql/*.ts',
+  documents: 'src/graphql/*.graphql',
   generates: {
     'src/generated/index.ts': {
       plugins: [
