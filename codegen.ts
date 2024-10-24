465d78b6b581d4eb44d27a9300cea5d412a7dbbf
Eason Sun <ballentin133@outlook.com>
2024-10-24 22:42:46 +0800

    first commit

---
+++ codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://localhost:3000/graphql',
+  documents: 'src/graphql/*.ts',
+  generates: {
+    'src/generated/index.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      hooks: {
+        afterOneFileWrite: 'prettier --write',
+      },
+    },
+  },
+}
+
+export default config
